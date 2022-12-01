4f61bee2300697d4f413adef35010aad8f5ab543
Wongje4 <>
2022-12-01 14:50:03 +0800

    feat: say fizz when multiple3_number

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,9 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int order) {
-
+        if (order % 3 == 0) {
+            return "Fizz";
+        }
         return Integer.toString(order);
     }
 }
