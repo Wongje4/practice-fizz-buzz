7511579a94f2f15760035579b8d732904ec5405f
Wongje4 <>
2022-12-01 15:07:31 +0800

    feat: say FizzBuzzWhizz when multiple 3,5,7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,14 +3,15 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int order) {
-        if (order % 3 == 0 && order % 5 == 0) {
+        if (order % 3 == 0 && order % 5 == 0 && order % 7 == 0) {
+            return "FizzBuzzWhizz";
+        } else if (order % 3 == 0 && order % 5 == 0) {
             return "FizzBuzz";
-        }
-        else if (order % 3 == 0) {
+        } else if (order % 3 == 0) {
             return "Fizz";
-        }else if(order % 5 == 0){
+        } else if (order % 5 == 0) {
             return "Buzz";
-        }else if(order % 7 == 0){
+        } else if (order % 7 == 0) {
             return "Whizz";
         }
         return Integer.toString(order);
